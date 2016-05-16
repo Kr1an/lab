1a76ec971b898a92999d194b177fea5f804e46f9
Kr1an <Kr1an@hotmail.com>
2016-05-16 18:36:14 +0300

    exFloat

---
+++ ConsoleApplication2/NewFloatType.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+
+	class NewFloatType : IFormattable , IEquatable<NewFloatType>
+	{
+		 
+		public delegate void testDelegate(int i);
+		
+		public event testDelegate testEvent;
+
+		public void count(int i)
+		{
+			if (i == 0)
+			{
+				testEvent(i);
+			}
+		}
+
+		public void message(int i)
+		{
+			Console.WriteLine("testDelegate event" + i);
+			return;
+		}
+		public float newFloat { get; set; }
+
+		public NewFloatType(float newFloat)
+		{
+			this.newFloat = newFloat;
+		}
+
+		public string ToString(string format, IFormatProvider formatProvider)
+		{
+			return Convert.ToString(this.newFloat);
+		}
+
+		public bool Equals(NewFloatType other)
+		{
+			return this.newFloat.Equals(other.newFloat);
+		}
+		public bool Equals(NewFloatType other, int place)
+		{
+			String[] str1 = new string[2];
+			char[] temp = new char[1];
+			temp[0] = '.';
+
+			str1 = Convert.ToString(this.newFloat).Split(temp, StringSplitOptions.RemoveEmptyEntries);
+			Console.WriteLine(str1[0] + "  " + str1[1]);
+			return true;
+		}
+
+		static public NewFloatType operator +(NewFloatType firstNum, NewFloatType secNum)
+		{
+			return new NewFloatType(firstNum.newFloat + secNum.newFloat);
+		}
+		static public NewFloatType operator -(NewFloatType firstNum, NewFloatType secNum)
+		{
+			return new NewFloatType(firstNum.newFloat - secNum.newFloat);
+		}
+		static public NewFloatType operator /(NewFloatType firstNum, NewFloatType secNum)
+		{
+			return new NewFloatType(firstNum.newFloat / secNum.newFloat);
+		}
+		static public NewFloatType operator *(NewFloatType firstNum, NewFloatType secNum)
+		{
+			return new NewFloatType(firstNum.newFloat * secNum.newFloat);
+		}
+		static public NewFloatType operator !(NewFloatType secNum)
+		{
+			return ((secNum.newFloat>0)?(new NewFloatType(0)):(new NewFloatType(1)));
+		}
+		static public NewFloatType operator ++(NewFloatType firstNum)
+		{
+			return new NewFloatType(firstNum.newFloat+1);
+		}
+		static public NewFloatType operator --(NewFloatType firstNum)
+		{
+			return new NewFloatType(firstNum.newFloat-1);
+		}
+
+		public static implicit operator double(NewFloatType v)
+		{
+			return v.newFloat;
+		}
+		public static implicit operator int(NewFloatType v)
+		{
+			return (int)v.newFloat;
+		}
+		public static explicit operator float(NewFloatType v)
+		{
+			return (float)v.newFloat;
+		}
+	}
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var ana = new { a = 34, b = "qwe", c = true };
+			Console.WriteLine(ana.a);
+			NewFloatType obj = new NewFloatType((float)45.23);
+			NewFloatType obj1 = new NewFloatType((float)45.23);
+			Dictionary<String, String> dic1 = new Dictionary<string, string>();
+			dic1.Add("qwe", "йцу");
+			dic1.Add("asd", "фыв");
+			Console.WriteLine(dic1["qwe"]);
+			dic1.Remove("qwe");
+			Console.WriteLine(dic1["qwe"]);
+			NewFloatType obj3 = obj + obj1;
+			Console.WriteLine(obj.ToString());
+			float temp = (float)obj3;
+			Console.WriteLine(temp);
+			obj.testEvent += obj.message;
+			try
+			{
+				obj.count(0);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Exception");
+			}
+			//obj.testEvent += obj.message;
+			
+		}
+	}
+}
