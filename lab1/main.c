3ed1dc5f6955093316bd8b9cee4a2c4a0c7bfdbd
Kr1an <Kr1an@hotmail.com>
2016-05-20 22:40:27 +0300

    lab1,lab2

---
+++ lab1/main.c
@@ -0,0 +1,89 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+
+static void int_to_rim(int num)
+{
+	int i = 0;
+	int n = 0;
+	wchar_t dig = (wchar_t)(0x1E8C);
+
+	if( num < 0 )
+	{
+		printf("-");
+		num *= (-1);
+	}
+	n = num / 10000;
+	num %= 10000;
+	for(i = 0; i < n; i ++ )
+		printf("%lc", dig); 
+	
+	dig = (wchar_t)(0x1E7C);
+	n = num / 5000;
+	num %= 5000;
+	for(i = 0; i < n ; i ++ )
+		printf("%lc", dig);
+	
+	dig = L'M';
+	n = num / 1000;
+	num %= 1000;
+	for( i = 0; i < n; i ++ )
+		printf("%lc", dig);
+
+	dig = (wchar_t)(0x216E);
+	n = num / 500;
+	num %= 500;
+	for( i = 0; i < n; i ++ )
+		printf("%lc", dig);
+
+	dig = (wchar_t)(0x216D);
+	n = num / 100;
+	num %= 100;
+	for( i = 0; i < n; i ++ )
+		printf("%lc", dig);
+	
+	dig = (wchar_t)(0x216C);
+	n = num / 50;
+	num %= 50;
+	for( i = 0; i < n; i ++ )
+		printf("%lc", dig);
+	
+	dig = (wchar_t)(0x2169);
+	n = num / 10;
+	num %= 10;
+	for( i = 0; i < n; i ++ )
+		printf("%lc", dig);
+	
+	dig = (wchar_t)(0x2164);
+	n = num / 5;
+	num %= 5;
+	for( i = 0; i < n; i ++ )
+		printf("%lc", dig);
+
+	dig = (wchar_t)(0x2160);
+	n = num / 1;
+	num %= 1;
+	for( i = 0; i < n; i ++ )
+		printf("%lc", dig);
+
+	printf("\n");
+	return;
+}
+
+int main( /*@unused@ */ int argc, /*@unused@ */ char *argv[])
+{
+	int num = 0;
+	(void)setlocale(LC_ALL, "");
+	printf("Enter the num(XXXXXXXX<num<XXXXXXXX): ");
+	if( scanf("%8d", &num) == 0){
+		printf("Error input...\n");
+		(void)getchar();
+		return 1;
+	}
+	printf("number in rim form: ");
+	int_to_rim(num);
+	return 0 ;
+}
+
---
+++ lab2/main.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct _Tetrahedron{
+	int sightLen;
+	unsigned sightLenState:1;
+}Tetrahedron;
+
+static const int VAR_DEFAULT = -1;
+
+static void clrscr(void);
+static void set_sightLen(Tetrahedron *obj);
+static void info();
+static void requestNew( /*@out@ */Tetrahedron *obj);
+static void printMenu(Tetrahedron obj);
+
+static int count_sumedUp(Tetrahedron	obj);
+static double count_squere(Tetrahedron	obj);
+static double count_volume(Tetrahedron	obj);
+static double count_hight(Tetrahedron	obj);
+static double count_radInSphere(Tetrahedron	obj);
+
+
+
+
+int main( /*@unused@ */ int argc, /*@unused@ */ char *argv[])
+{
+	Tetrahedron obj;
+	requestNew(&obj);
+
+	do {
+		printMenu(obj);
+		switch (getchar()) {
+		case '1':
+			set_sightLen(&obj);
+			break;
+		case '7':
+			info();
+			break;
+		case '8':
+			return 0;
+		default:
+			break;
+		}
+		(void)getchar();
+	} while (1);
+	return 0;
+}
+
+/*================FUNCTIONS======================================================*/
+
+
+static double count_radInSphere(Tetrahedron	obj)
+{
+	return count_hight(obj) / 3.0;
+}
+static double count_hight(Tetrahedron	obj)
+{
+	return sqrt(2.0/3) * (double)obj.sightLen;
+}
+static double count_volume(Tetrahedron	obj)
+{
+	return (double)pow(obj.sightLen, 3)/(6 * sqrt(2));
+}
+static double count_squere(Tetrahedron	obj)
+{
+	return sqrt(3) * (double)pow(obj.sightLen, 2);
+}
+static int count_sumedUp(Tetrahedron obj)
+{
+	return 6 * obj.sightLen;
+}
+
+static void info()
+{
+	(void)getchar();
+	clrscr();
+	printf("Info : \nprogram: Tetrahedron \nversion: 1.0 \n \tAutor: Kruan \n \tMail: Kruan@gmail.com");
+}
+
+static void requestNew(Tetrahedron *obj)
+{
+	obj->sightLen = VAR_DEFAULT;
+	obj->sightLenState = 0;
+	return;
+}
+
+void clrscr(void)
+{
+	int i;
+	for (i = 0; i < 30; i++)
+		printf("\n");
+}
+
+static void set_sightLen(Tetrahedron *obj)
+{
+	
+	char temp[20];
+	int sightLen = 0;
+	(void)getchar();
+	clrscr();
+	while (1) {
+		printf("Set one-line length (-1+Enter)-exit: ");
+		(void)scanf("%s", temp);
+		sightLen = atoi(temp);
+		if (sightLen == VAR_DEFAULT && strlen(temp)< 10){
+			return;
+		} else if (sightLen > 0 && strlen(temp)<10) {
+			obj->sightLen = sightLen;
+			obj->sightLenState = 1;
+			return;
+		} else if(strlen(temp)>10)
+			printf("Len Error; Try one more\n");
+	}
+}
+static void printMenu(Tetrahedron obj)
+{
+	clrscr();
+	printf("\t\t\tTetrahedron constructor\n");
+	printf("Information about your Tetrahedron model:\n");
+	printf("  1: The length of one sight line(mm): %d\n",
+	       (obj.sightLenState == 0) ? (-1) : (obj.sightLen));
+	printf("Show compleat information about your model: \n");
+	printf("  2: Sumed up length of all lines(mm): %d\n", (obj.sightLenState == 0) ? (-1) : (count_sumedUp(obj)));
+	printf("  3: Squere of all sights(mm^2): %lf\n", (obj.sightLenState == 0) ? (-1) : (count_squere(obj)));
+	printf("  4: Volume of model(mm^3): %lf\n", (obj.sightLenState == 0) ? (-1) : (count_volume(obj)));
+	printf("  5: Hight of model(mm): %lf\n", (obj.sightLenState == 0) ? (-1) : (count_hight(obj)));
+	printf("  6: Radiuse of in-sphere(mm): %lf\n", (obj.sightLenState == 0) ? (-1) : (count_radInSphere(obj)));
+	printf("  7: Info.\n");
+	printf("  8: Exit.\n");
+	printf("\t\tPress one of 1..8+Enter button and...\n");
+
+}
